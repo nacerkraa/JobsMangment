9842900d736abbbfb4e745f085af0b92ffdfccc1
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-11-15 19:53:45 +0100

    Hello world application with spring boot and jsp

---
+++ src/main/java/com/example/demo/MagProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagProjectApplication.class, args);
+	}
+
+}
