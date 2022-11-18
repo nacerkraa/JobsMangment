f043b8752c531f9c43d5ac0c258f970e247e6ba5
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-11-19 00:00:13 +0100

    Create Table User

--- mag_db.sql
+++ mag_db.sql
@@ -1,1 +1,3 @@
 create database mag_db;
+
+CREATE TABLE `User` (id bigint(20) NOT NULL AUTO_INCREMENT, nom varchar(255), prenom varchar(255), email varchar(255) NOT NULL UNIQUE, motPasse varchar(255), phone int(10), sexe varchar(15), age varchar(255), location varchar(255), type varchar(255), PRIMARY KEY (id));
